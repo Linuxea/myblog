57227ea7376dd903e041cb296db7d2a49112a0c4
linuxea <linuxea@foxmail.com>
2017-09-11 00:46:15 +0800

    add bootstrap js and files...

---
+++ src/main/java/com/linuxea/exception/BlogException.java
@@ -0,0 +1,27 @@
+package com.linuxea.exception;
+
+/**
+ * myblog exception
+ * Created by Linuxea on 2017-09-11.
+ */
+public class BlogException extends RuntimeException {
+
+    public BlogException() {
+    }
+
+    public BlogException(String message) {
+        super(message);
+    }
+
+    public BlogException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BlogException(Throwable cause) {
+        super(cause);
+    }
+
+    public BlogException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
