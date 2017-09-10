e1dd7925cbc7cf6179eeddf94e4a54020988a244
linuxea <linuxea@foxmail.com>
2017-09-11 00:31:51 +0800

    update sql script

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -1,25 +1,38 @@
 create database myblog;
-
-
 use myblog;
-
 
 create table kind(
 	id varchar(36) not null,
 	name varchar(50) not null,
+	create_time datetime,
 	primary key(id)
-)
+);
 
 create table article(
 	id varchar(36) not null,
 	name varchar(50) not null,
 	create_time datetime,
 	primary key(id)
-)
+);
 
 create table tag(
 	id varchar(36) not null,
 	name varchar(50) not null,
 	create_time datetime,
 	primary key(id)
-)
+);
+
+
+create table article_with_kind(
+	id varchar(36) not null,
+	article_id  varchar(36) not null,
+	kind_id  varchar(36) not null,
+	primary key(id)
+);
+
+create table article_with_tag(
+	id varchar(36) not null,
+	article_id  varchar(36) not null,
+	tag_id  varchar(36) not null,
+	primary key(id)
+);
