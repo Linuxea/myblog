06975dedad212c97d82a6514fb060271aeff3fcb
linuxea <linuxea@foxmail.com>
2017-09-11 13:04:32 +0800

    exception log

--- src/main/java/com/linuxea/interceptor/ExceptionInterceptor.java
+++ src/main/java/com/linuxea/interceptor/ExceptionInterceptor.java
@@ -3,11 +3,15 @@ package com.linuxea.interceptor;
 
 import com.jfinal.aop.Interceptor;
 import com.jfinal.aop.Invocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Linuxea on 2017-09-10.
  */
 public class ExceptionInterceptor implements Interceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionInterceptor.class);
 
     @Override
     public void intercept(Invocation inv) {
@@ -16,6 +20,14 @@ public class ExceptionInterceptor implements Interceptor {
             inv.invoke();
         } catch (Exception e) {
             e.printStackTrace();
+
+            StringBuilder sb = new StringBuilder("\n---Exception Log Begin---\n");
+            sb.append("Controller:").append(inv.getController().getClass().getName()).append("\n");
+            sb.append("Method:").append(inv.getMethodName()).append("\n");
+            sb.append("Exception Type:").append(e.getClass().getName()).append("\n");
+            sb.append("Exception Details:");
+            logger.error(sb.toString(), e);
+
         }
 
     }
