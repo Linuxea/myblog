ffedfc5bbe754ddfcb405d47cb98f2b570bbd881
linuxea <linuxea@foxmail.com>
2017-09-18 15:34:21 +0800

    update sql scripts

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -1,6 +1,8 @@
+DROP DATABASE IF EXISTS myblog;
 CREATE DATABASE myblog;
 USE myblog;
 
+DROP TABLE IF EXISTS kind;
 CREATE TABLE kind (
   id          VARCHAR(36) NOT NULL,
   name        VARCHAR(50) NOT NULL,
@@ -10,7 +12,7 @@ CREATE TABLE kind (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-
+DROP TABLE IF EXISTS article;
 CREATE TABLE `article` (
   `id`          VARCHAR(36) NOT NULL,
   `title`       VARCHAR(50) NOT NULL,
@@ -22,6 +24,7 @@ CREATE TABLE `article` (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
+DROP TABLE IF EXISTS tag;
 CREATE TABLE tag (
   id          VARCHAR(36) NOT NULL,
   name        VARCHAR(50) NOT NULL,
@@ -32,6 +35,7 @@ CREATE TABLE tag (
   DEFAULT CHARSET = utf8;
 
 
+DROP TABLE IF EXISTS article_with_kind;
 CREATE TABLE article_with_kind (
   id         VARCHAR(36) NOT NULL,
   article_id VARCHAR(36) NOT NULL,
@@ -41,6 +45,7 @@ CREATE TABLE article_with_kind (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
+DROP TABLE IF EXISTS article_with_tag;
 CREATE TABLE article_with_tag (
   id         VARCHAR(36) NOT NULL,
   article_id VARCHAR(36) NOT NULL,
@@ -50,6 +55,7 @@ CREATE TABLE article_with_tag (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
+DROP TABLE IF EXISTS user;
 CREATE TABLE user (
   id            VARCHAR(36) NOT NULL,
   user_name     VARCHAR(36) NOT NULL,
@@ -59,6 +65,8 @@ CREATE TABLE user (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
+
+DROP TABLE IF EXISTS login_list;
 CREATE TABLE login_list (
   id            VARCHAR(36) NOT NULL,
   ip            VARCHAR(36) NOT NULL,
@@ -69,6 +77,8 @@ CREATE TABLE login_list (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
+
+DROP TABLE IF EXISTS black_list;
 CREATE TABLE black_list (
   id     VARCHAR(36)  NOT NULL,
   ip     VARCHAR(36)  NOT NULL,
